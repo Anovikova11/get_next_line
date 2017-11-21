b5198777c3a3b640d466d52b7b8255b432dfee9a
Anovikova11 <anovikova11@gmail.com>
2017-11-21 15:27:56 -0800

    added mult fd

--- nofd.c
+++ nofd.c
@@ -1,12 +1,12 @@
-/* ************************************************************************** */
+  /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stuff.c                                            :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alnoviko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/16 21:00:46 by alnoviko          #+#    #+#             */
-/*   Updated: 2017/11/16 21:00:49 by alnoviko         ###   ########.fr       */
+/*   Created: 2017/11/06 22:18:16 by alnoviko          #+#    #+#             */
+/*   Updated: 2017/11/14 15:43:21 by alnoviko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,30 +39,38 @@ int	ft_read_file(int fd, char **string)
 	return (ret);
 }
 
+/*
+** 2. Go through string and find the first new line.
+** 3. Set pointer to '\0' in order to copy first found line to the buf.
+** First found line = *line = part of string before '\0'.
+** 3. Set a pointer to the begining of second line to skip first found line.
+** 4. Itarate through string again to find a second line, ending with '\n'.
+** 4. What to do with OEF? *line = str[fd]
+*/
 
 int	get_next_line(const int fd, char **line)
 {
-	static char 	*string[5000]; //array of 5000 strings(lines, slots)
+	static char 	*string; //array of 5000 strings(lines, slots)
 	char 			*pointer;
 	char 			*tmp; // *tmp = sasha, tmp[fd] = sasha, tmp[fd][0] = s
 	
-	if (ft_read_file(fd, &string[fd]) < 0 || !line || !BUFF_SIZE)
+	if (ft_read_file(fd, &string) < 0 || !line || !BUFF_SIZE)
 		return (-1);
 	
-	if ((pointer = ft_strchr(string[fd], '\n'))) //return where fisrt nl is
+	if (*string && (pointer = ft_strchr(string, '\n'))) //return where fisrt nl is
 	{
-		tmp = string[fd];
+		tmp = string;
 		*pointer = '\0'; //set an end of the first line 
-		*line = ft_strdup(string[fd]); //copy result before first newline to a line.
-		string[fd] = ft_strdup(pointer + 1); //copy result starting from first char of the second line.
+		*line = ft_strdup(string); //copy result before first newline to a line.
+		string = ft_strdup(pointer + 1); //copy result starting from first char of the second line.
 		ft_strdel(&tmp);
 		return (1);
 	}
-	else if (*string[fd] && (pointer = ft_strchr(string[fd], '\0'))) //if no nl, but string exists, search for the EOF.
+	else if (*string && (pointer = ft_strchr(string, '\0')))
 	{
 		// tmp = string;
-		*line = ft_strdup(string[fd]); //copy result before EOF to a line.
-		string[fd] = ft_strdup(pointer);
+		*line = ft_strdup(string); //copy result before EOF to a line.
+		string = ft_strdup(pointer);
 		// ft_strdel(&tmp);
 		return (1);
 	}
